483e89b8d8dec86ef7ad352e14e1445b71cacddf
sahilajaz <sahilajazw@gmail.com>
2024-10-21 22:08:00 +0530

    loader added

---
+++ src/components/QuestionCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const QuestionCard = ({question}) => {
+
+    console.log(question.question)
+    
+  return (
+    <div className="conatiner">
+        
+    </div>
+  )
+}
+
+export default QuestionCard
--- src/components/QuestionsScreen.jsx
+++ src/components/QuestionsScreen.jsx
@@ -1,26 +1,45 @@
 import {useState , useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import axois from "axios";
+import axios from "axios";
+import Loader from './Loader';
+import QuestionCard from './QuestionCard';
+
 
 const QuestionsScreen = () => {
    const[questions , setQuestions] = useState()
-   const { categoryNumber } = useParams(); 
+   const { categoryNumber } = useParams() 
 
+   
    const baseUrl = `https://opentdb.com/api.php?amount=10&category=${categoryNumber}`;  
    const anyCateoryBaseUrl = 'https://opentdb.com/api.php?amount=10'
 
-
+   const fetchQuestions = async () => {
+    try {
+      const res = categoryNumber == 0 
+        ? await axios.get(anyCateoryBaseUrl)
+        : await axios.get(baseUrl);
+      setQuestions(res.data.results);
+    } catch (error) {
+      console.error('Error fetching questions:', error);
+    }
+  }
+   
+   
+     
     useEffect(() => {
-       categoryNumber == 0 ? axois.get(anyCateoryBaseUrl).then((res) => setQuestions(res.data.results)) 
-       : axois.get(baseUrl).then((res) => setQuestions(res.data.results))
+       fetchQuestions()
     } , []) 
     
     
     
   return (
-    <div>
-      
-    </div>
+    questions ? (
+      questions.map((question, index) => (
+        <QuestionCard key={index} question={question} />
+      ))
+    ) : (
+      <Loader />
+    )
   )
 }
 
