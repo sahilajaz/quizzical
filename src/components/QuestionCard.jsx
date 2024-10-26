d3dbb39560b1ce2d81189695641ef8dd92f2c6b5
sahilajaz <sahilajazw@gmail.com>
2024-10-26 16:22:24 +0530

    changes made

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,29 +1,67 @@
-import React from 'react';
+import {useState} from 'react';
 import he from 'he';
 
-const QuestionCard = ({ question, correctAns }) => {
-  const { correct_answer, incorrect_answers } = question;
 
-  const allAnswers = [correct_answer, ...incorrect_answers];
+const QuestionCard = ({ data }) => {
+  const[clicked , setClicked] = useState(null)
 
+  const optionBtnClicked = (id) => {
+    setClicked(id)
+  }
+
+  const ccc = (id) => {
+      console.log(id)
+  }
+
+ 
+
+  
   return (
-    <div className='container py-2'>
-      <div>
-        <p className="text-lg mb-2">{he.decode(question.question)}</p>
-        <div className="btn flex flex-wrap gap-2">
-          {allAnswers.map((answer, index) => (
-            <button 
-              key={index} 
-              className='border border-[#293264] w-auto  min-w-[50px] h-[30px] rounded-lg mb-3 text-[0.8rem] px-2 py-1 hover:bg-[#d3d3d3] transition-colors duration-200'
-            >
-              {he.decode(answer)}
-            </button>
-          ))}
-          <div className='w-[700px]  border-[0.79px] border-[#DBDEF0]'/>
-        </div>
+    <div className="container py-2 border-b-[1px] border-[#DBDEF0]">
+      <p className="text-xl font-semibold mb-2">{he.decode(data.question)}</p>
+      <div className="flex flex-wrap gap-2">
+        {data.questionOptions.map((option, i) => (
+          <button
+            key={i}
+            className={`border border-[#293264] w-auto min-w-[50px] h-[30px] rounded-lg mb-3 text-[0.8rem] px-2 py-1 ${clicked === i ? 'bg-[#d3d3d3]' : 'hover:bg-[#d3d3d3]'} transition-colors duration-200`}
+            onClick={() => {
+              optionBtnClicked(i) , 
+              ccc(data.id)
+            }}
+          >
+            {he.decode(option)}
+          </button>
+        ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default QuestionCard;
+
+
+ 
+  
+
+
+  
+
+  
+ 
+
+  
+
+
+
+// {allAnswer.map((answer, index) => (
+//   <button 
+//     key={index} 
+//     className={
+//       clickedIdx === index ? 'border border-[#293264] w-auto  min-w-[50px] h-[30px] rounded-lg mb-3 text-[0.8rem] px-2 py-1 bg-[#d3d3d3] transition-colors duration-200' :
+//       'border border-[#293264] w-auto  min-w-[50px] h-[30px] rounded-lg mb-3 text-[0.8rem] px-2 py-1 hover:bg-[#d3d3d3] transition-colors duration-200'
+//     }
+//     onClick={()=>setClickedIdx(index)} 
+//   >
+//     {he.decode(answer)}
+//   </button>
+// ))}
--- src/components/QuestionsScreen.jsx
+++ src/components/QuestionsScreen.jsx
@@ -3,77 +3,82 @@ import { useParams } from 'react-router-dom';
 import axios from "axios";
 import Loader from './Loader';
 import QuestionCard from './QuestionCard';
+import {nanoid} from 'nanoid'
 
 
 const QuestionsScreen = () => {
-   const[questions , setQuestions] = useState()
+   const[questiondata , setQuestionData] = useState()
    const { categoryNumber } = useParams() 
   
-
-  
-   
-
-   const baseUrl = `https://opentdb.com/api.php?amount=10&category=${categoryNumber}`;  
+  const baseUrl = `https://opentdb.com/api.php?amount=10&category=${categoryNumber}`  
    const anyCateoryBaseUrl = 'https://opentdb.com/api.php?amount=10'
 
    const fetchQuestions = async () => {
     try {
       const res = categoryNumber == 0 
         ? await axios.get(anyCateoryBaseUrl)
-        : await axios.get(baseUrl);
-      setQuestions(res.data.results);
+        : await axios.get(baseUrl)
+        
+        const formattedQuestions = res.data.results.map(q => {
+          q.questionOptions = [...q.incorrect_answers, q.correct_answer].sort(() => Math.random() - 0.5);
+          q.id = nanoid();
+          return q
+        })
+        
+  
+      setQuestionData(formattedQuestions)
     } catch (error) {
-      console.error('Error fetching questions:', error);
+      console.error('Error fetching questions:', error)
     }
-  }
-   
-   
-     
+  };
+  
+
     useEffect(() => {
        fetchQuestions()
     } , []) 
+
     
     
-    const correctAns = questions ? questions.map(q => q.correct_answer) :   []
+    
   
    const loadingScreen = "bg-[#f5f7fb] w-[300px] sm:w-[600px] flex flex-col items-center justify-center sm:h-[500px] h-[300px] mt-2"
    const questionScreen = "bg-[#f5f7fb] w-[330px] sm:w-[700px] h-auto mt-2 relative"
     
   
    return (
-    <div className={questions ? questionScreen : loadingScreen}>
-        <div className='py-1 px-10 relative'>
-
-         {
-          questions && (
-            <img
-           src="../src/assets/yellow.png"
-          className="h-9 sm:h-16 absolute left-[290px] sm:left[420px]  md:left-[626px] top-[0.5px]"
-        />
-          )
-         }
-
-        {
-            questions ? (
-                questions.map((question, index) => (
-                    <QuestionCard key={index} question={question} correctAns={correctAns} />
-                ))
-            ) : (
-                <Loader />
-            )
-        }
-        </div>
-         <div className='w-full py-4 flex justify-center relative'>
-
-          {
-           questions && (
-            <button  
-            className="bg-[#4D5B9E] text-white px-5 py-1 rounded-xl font-bold tex-sm" onClick={fetchQuestions}> New Quiz</button>
-           ) 
-          }
-         </div>
+    <div className={questiondata && questiondata.length > 0 ? questionScreen : loadingScreen}>
+      <div className='py-1 px-10 relative'>
+        {questiondata && questiondata.length > 0 && (
+          <img
+            src="../src/assets/yellow.png"
+            className="h-9 sm:h-16 absolute left-[290px] sm:left[420px]  md:left-[626px] top-[0.5px]"
+          />
+        )}
+  
+        {questiondata && questiondata.length > 0 ? (
+          questiondata.map((question, index) => (
+            <QuestionCard 
+              key={index} 
+              data={question}  
+            />
+          ))
+        ) : (
+          <Loader />
+        )}
+      </div>
+      <div className='w-full py-4 flex justify-center relative'>
+        {questiondata && questiondata.length > 0 && (
+          <button  
+            className="bg-[#4D5B9E] text-white px-5 py-1 rounded-xl font-bold tex-sm" 
+            onClick={fetchQuestions}
+          >
+            New Quiz
+          </button>
+        )}
+      </div>
     </div>
-);
+  );
+  
 
 }
 
