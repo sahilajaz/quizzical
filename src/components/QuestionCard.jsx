ed80fa3d159d12bf9abff76fa421692521f9f2d7
sahilajaz <sahilajazw@gmail.com>
2024-10-26 21:55:21 +0530

    added

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,67 +1,55 @@
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
 import he from 'he';
 
+const QuestionCard = ({ data, OptionSelection, selectedOption, quizSubmit }) => {
+  const [clicked, setClicked] = useState(null)
 
-const QuestionCard = ({ data }) => {
-  const[clicked , setClicked] = useState(null)
+  const options = data.questionOptions.map((option) => {
+    const isSelected = selectedOption.find((e) => (
+      e.questionId === data.id && e.option === option
+    ))
 
-  const optionBtnClicked = (id) => {
-    setClicked(id)
-  }
+    const correctOption = quizSubmit && data.correct_answer === option
+    const incorrectOption = quizSubmit && isSelected && data.correct_answer !== isSelected.option
 
-  const ccc = (id) => {
-      console.log(id)
-  }
+    
+    let bgColor = ''
+    if (isSelected && !quizSubmit) {
+      bgColor = "#D6DBF5"
+    } else if (quizSubmit && correctOption) {
+      bgColor = "#94D7A2"
+    } else if (quizSubmit && incorrectOption) {
+      bgColor = "#F8BCBC"
+    }
 
+    return { option, bgColor }
+  })
+
+  useEffect(() => {
+    setClicked(null)
+  }, [quizSubmit])
  
-
-  
+   console.log(options)
   return (
     <div className="container py-2 border-b-[1px] border-[#DBDEF0]">
       <p className="text-xl font-semibold mb-2">{he.decode(data.question)}</p>
       <div className="flex flex-wrap gap-2">
-        {data.questionOptions.map((option, i) => (
+        {options.map((optionData, i) => (
           <button
             key={i}
-            className={`border border-[#293264] w-auto min-w-[50px] h-[30px] rounded-lg mb-3 text-[0.8rem] px-2 py-1 ${clicked === i ? 'bg-[#d3d3d3]' : 'hover:bg-[#d3d3d3]'} transition-colors duration-200`}
+            style={{ backgroundColor: clicked === i && !quizSubmit ? '#d3d3d3' : optionData.bgColor }}
+            className={`border border-[#293264] w-auto min-w-[50px] h-[30px] rounded-lg mb-3 text-[0.8rem] px-2 py-1 transition-colors duration-200`}
             onClick={() => {
-              optionBtnClicked(i) , 
-              ccc(data.id)
+              setClicked(i);
+              OptionSelection(data.id, optionData.option);
             }}
           >
-            {he.decode(option)}
+            {he.decode(optionData.option)}
           </button>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default QuestionCard;
-
-
- 
-  
-
-
-  
-
-  
- 
-
-  
-
-
-
-// {allAnswer.map((answer, index) => (
-//   <button 
-//     key={index} 
-//     className={
-//       clickedIdx === index ? 'border border-[#293264] w-auto  min-w-[50px] h-[30px] rounded-lg mb-3 text-[0.8rem] px-2 py-1 bg-[#d3d3d3] transition-colors duration-200' :
-//       'border border-[#293264] w-auto  min-w-[50px] h-[30px] rounded-lg mb-3 text-[0.8rem] px-2 py-1 hover:bg-[#d3d3d3] transition-colors duration-200'
-//     }
-//     onClick={()=>setClickedIdx(index)} 
-//   >
-//     {he.decode(answer)}
-//   </button>
-// ))}
--- src/components/QuestionsScreen.jsx
+++ src/components/QuestionsScreen.jsx
@@ -1,85 +1,129 @@
-import {useState , useEffect} from 'react';
-import { useParams } from 'react-router-dom';
-import axios from "axios";
-import Loader from './Loader';
-import QuestionCard from './QuestionCard';
-import {nanoid} from 'nanoid'
-
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from "axios"
+import Loader from './Loader'
+import QuestionCard from './QuestionCard'
+import { nanoid } from 'nanoid'
 
 const QuestionsScreen = () => {
-   const[questiondata , setQuestionData] = useState()
-   const { categoryNumber } = useParams() 
-  
-  const baseUrl = `https://opentdb.com/api.php?amount=10&category=${categoryNumber}`  
-   const anyCateoryBaseUrl = 'https://opentdb.com/api.php?amount=10'
+  const [questiondata, setQuestionData] = useState([])
+  const [selectedOption, setSelectedOption] = useState([])
+  const [quizSubmit, setQuizSubmit] = useState(false)
+  const [countCorrect, setCountCorrect] = useState(0)
+  const [resetQuiz, setResetQuiz] = useState(false) 
+  const [loading, setLoading] = useState(true) 
+  const { categoryNumber } = useParams()
 
-   const fetchQuestions = async () => {
+  const baseUrl = `https://opentdb.com/api.php?amount=10&category=${categoryNumber}`
+  const anyCategoryBaseUrl = 'https://opentdb.com/api.php?amount=10'
+
+  const fetchQuestions = async () => {
+    setLoading(true) 
     try {
       const res = categoryNumber == 0 
-        ? await axios.get(anyCateoryBaseUrl)
+        ? await axios.get(anyCategoryBaseUrl)
         : await axios.get(baseUrl)
-        
-        const formattedQuestions = res.data.results.map(q => {
-          q.questionOptions = [...q.incorrect_answers, q.correct_answer].sort(() => Math.random() - 0.5);
-          q.id = nanoid();
-          return q
-        })
-        
-  
+
+      const formattedQuestions = res.data.results.map(q => {
+        q.questionOptions = [...q.incorrect_answers, q.correct_answer].sort(() => Math.random() - 0.5)
+        q.id = nanoid()
+        return q
+      })
       setQuestionData(formattedQuestions)
+      setSelectedOption([]) 
+      setQuizSubmit(false)
+      setResetQuiz(prev => !prev) 
     } catch (error) {
       console.error('Error fetching questions:', error)
+    } finally {
+      setLoading(false)
     }
-  };
+  }
+
+  const OptionSelection = (questionId, option) => {
+    setSelectedOption(prevOption => {
+      const newOption = prevOption.filter(e => e.questionId !== questionId)
+      newOption.push({ questionId, option }) 
+      return newOption
+    })
+  }
+
+  useEffect(() => {
+    fetchQuestions()
+  }, [])
+
+  useEffect(() => {
+    if (quizSubmit) {
+      let count = 0;
+      selectedOption.forEach(option => {
+        const correctOption = questiondata.find(e => 
+          e.id === option.questionId && e.correct_answer === option.option
+        );
+        if (correctOption) count++;
+      });
+      setCountCorrect(count);
+    }
+  }, [quizSubmit, questiondata, selectedOption]);
   
 
-    useEffect(() => {
-       fetchQuestions()
-    } , []) 
+  const loadingScreen = "bg-[#f5f7fb] w-[300px] sm:w-[600px] flex flex-col items-center justify-center sm:h-[500px] h-[300px] mt-2"
+  const questionScreen = "bg-[#f5f7fb] w-[330px] sm:w-[700px] h-auto mt-2 relative"
 
-    
-    
-    
-  
-   const loadingScreen = "bg-[#f5f7fb] w-[300px] sm:w-[600px] flex flex-col items-center justify-center sm:h-[500px] h-[300px] mt-2"
-   const questionScreen = "bg-[#f5f7fb] w-[330px] sm:w-[700px] h-auto mt-2 relative"
-    
-  
-   return (
-    <div className={questiondata && questiondata.length > 0 ? questionScreen : loadingScreen}>
+  return (
+    <div className={loading ? loadingScreen : questionScreen}>
       <div className='py-1 px-10 relative'>
-        {questiondata && questiondata.length > 0 && (
-          <img
-            src="../src/assets/yellow.png"
-            className="h-9 sm:h-16 absolute left-[290px] sm:left[420px]  md:left-[626px] top-[0.5px]"
-          />
-        )}
-  
-        {questiondata && questiondata.length > 0 ? (
+        {loading ? (
+          <Loader />
+        ) : (
           questiondata.map((question, index) => (
             <QuestionCard 
               key={index} 
-              data={question}  
+              data={question}
+              OptionSelection={OptionSelection}
+              quizSubmit={quizSubmit}
+              selectedOption={selectedOption}  
+              resetQuiz={resetQuiz}
             />
           ))
-        ) : (
-          <Loader />
         )}
       </div>
       <div className='w-full py-4 flex justify-center relative'>
-        {questiondata && questiondata.length > 0 && (
-          <button  
+        {
+          !loading && questiondata.length > 0 && questiondata.length === selectedOption.length && !quizSubmit && (
+            <button  
+              className="bg-[#4D5B9E] text-white px-5 py-1 rounded-xl font-bold tex-sm" 
+              onClick={() => setQuizSubmit(true)}
+            >
+              Check Answers
+            </button>
+          )
+        }
+        {
+          !loading && questiondata.length > 0 && questiondata.length !== selectedOption.length && !quizSubmit && (
+            <button  
+              className="bg-[#4D5B9E] text-white px-5 py-1 rounded-xl font-bold tex-sm" 
+              onClick={() => fetchQuestions()}
+            >
+              New Quiz
+            </button>
+          )
+        }
+        {
+          quizSubmit && !loading && (
+            <div className='flex gap-3 py-5'>
+            <h6>You scored {countCorrect}/{questiondata.length}</h6>
+            <button  
             className="bg-[#4D5B9E] text-white px-5 py-1 rounded-xl font-bold tex-sm" 
-            onClick={fetchQuestions}
-          >
-            New Quiz
-          </button>
-        )}
+            onClick={() => fetchQuestions()}
+            >
+              New Quiz
+            </button>
+           </div>
+          )
+        }
       </div>
     </div>
-  );
-  
-
+  )
 }
 
 export default QuestionsScreen
